8724bc17c2ce84061d0674e5bf7c6e0bfb6a3c72
Staffer Oliver <ly715000@live.cn>
2022-01-13 01:25:12 +0800

    Updating comments

--- src/rembg/bg.py
+++ src/rembg/bg.py
@@ -112,14 +112,29 @@ def remove(
     alpha_matting=False,
     *args, **kwargs
 ):
+    """
+    Background removal on a single image
+
+    :param data: the input image (PIL.Image or NumPy.ndarray)
+    :param model_name: the model used in the background removal
+    :param alpha_matting: if True, alpha matting will be applied with the rest of parameters
+    :return: return True if all the steps are correctly finished
+    """
+
+    # Initialize model
     model = get_model(model_name)
+
+    # Transform data
     if isinstance(data, np.ndarray):
         img = Image.open(io.BytesIO(data)).convert("RGB")
     else:
         img = data
+
+    # Predicting mask using u2net
     mask = Image.fromarray(detect.predict(model, np.array(img)) * 255).convert("L")
     cutout = None
 
+    # Generate output image
     if alpha_matting:
         cutout = alpha_matting_cutout(
                 img,
@@ -130,6 +145,7 @@ def remove(
     if cutout is None:
         cutout = naive_cutout(img, mask)
 
+    # Save the image
     bio = io.BytesIO()
     cutout.save(bio, "PNG")
     return Image.open(io.BytesIO(bio.getbuffer())).convert("RGBA")
@@ -142,15 +158,35 @@ def portrait(
     sigma=2,
     alpha=0.5
 ):
+    """
+    Portrait generation with single given picture
+
+    :param data: the input image (PIL.Image or NumPy.ndarray)
+    :param model_name: the model used in the portrait generation
+    :param composite: if True, the original portrait will be blurred and composite into the output
+    :param sigma: the blur parameters used for Gaussian filter (Used when composite=True)
+    :param alpha: the blur parameters used for Gaussian filter (Used when composite=True)
+    :return: generated portrait (In PIL.Image)
+    """
+
+    # Initialize model
     model = get_model(model_name)
+
+    # Transform data
     if isinstance(data, np.ndarray):
         img = data
     else:
         img = np.array(data)
+
+    # Size alert. If the picture is far too small, the result might be unexpected
     if img.shape[0] < 512 and img.shape[1] < 512:
         print("The size of the input picture is too small to generate. The result may be unexpected.")
         print("To obtain a good portrait, use a picture larger than 512*512 with a clear face.")
+
+    # Predicting portrait using u2net
     output = detect.predict(model, img, True)
+
+    # Generate output image
     if composite:
         output = transform.resize(output, img.shape[0:2],order=2)
         output = output/(np.amax(output)+1e-8) * 255
@@ -164,6 +200,13 @@ def portrait(
 
 
 def alpha_layer_remove(input_image, bg_color=np.array([255, 255, 255])):
+    """
+    Remove alpha layer in a PNG (RGBA color format) file/array
+
+    :param input_image: the input image (in PIL.Image or NumPy.ndarray)
+    :param bg_color: the color used to fill the blank left by alpha layer, default color is white
+    :return: processed picture(in NumPy.ndarray)
+    """
     if isinstance(input_image, np.ndarray):
         img = input_image
     else:
@@ -180,23 +223,42 @@ def video_remove(
     alpha_matting=False,
     *args, **kwargs
 ):
+    """
+    Video background removal
+
+    :param input_path: the path to the input video file
+    :param output_path: the path to the output video file
+    :param model_name: the model used in the background removal
+    :param alpha_matting: if True, alpha matting will be applied with the rest of parameters
+    :return: return True if all the steps are correctly finished
+    """
+
+    # Library detection
     try:
         import ffmpeg
     except ModuleNotFoundError:
         print("ffmpeg library is not currently installed, which is required for this functionality")
         print("Please run 'pip install opencv-python' and 'apt install ffmpeg' in command-line to install dependency")
         return False
+
+    # Obtain basic video infos
     probe = ffmpeg.probe(input_path)
     width = probe['streams'][0]['width']
     height = probe['streams'][0]['height']
     frame_rate = probe['streams'][0]['avg_frame_rate']
     data = ffmpeg.input(input_path)
+
+    # Extract frames
     video_frames = np.frombuffer(data
                                  .output('pipe:', format='rawvideo', pix_fmt='rgb24')
                                  .run(quiet=True)[0], np.uint8).reshape([-1, height, width, 3])
+
+    # Initailize output flow
     output = ffmpeg.input('pipe:', format='rawvideo', pix_fmt='rgb24', s='{}x{}'.format(width, height)) \
                    .output(output_path, r=frame_rate).overwrite_output() \
                    .run_async(pipe_stdin=True, quiet=True)
+
+    # Removal process
     model = get_model(model_name)
     for i in range(video_frames.shape[0]):
         img = Image.fromarray(video_frames[i, :, :, :]).convert('RGB')
@@ -211,6 +273,8 @@ def video_remove(
         if cutout is None:
             cutout = naive_cutout(img, mask)
         output.stdin.write(alpha_layer_remove(cutout).tobytes())
+
+    # Write & close flow
     output.stdin.close()
     output.wait()
     return True
@@ -223,23 +287,44 @@ def video_portrait(
     composite=False,
     sigma=2,
     alpha=0.5):
+    """
+    Video portrait generation (Experimental)
+
+    :param input_path: the path to the input video file
+    :param output_path: the path to the output video file
+    :param model_name: the model used in the portrait generation
+    :param composite: if True, the original portrait will be blurred and composite into the output
+    :param sigma: the blur parameters used for Gaussian filter (Used when composite=True)
+    :param alpha: the blur parameters used for Gaussian filter (Used when composite=True)
+    :return: return True if all the steps are correctly finished
+    """
+
+    # Library detection
     try:
         import ffmpeg
     except ModuleNotFoundError:
         print("ffmpeg library is not currently installed, which is required for this functionality")
         print("Please run 'pip install opencv-python' and 'apt install ffmpeg' in command-line to install dependency")
         return False
+
+    # Obtain basic video infos
     probe = ffmpeg.probe(input_path)
     width = probe['streams'][0]['width']
     height = probe['streams'][0]['height']
     frame_rate = probe['streams'][0]['avg_frame_rate']
     data = ffmpeg.input(input_path)
+
+    # Extract frames
     video_frames = np.frombuffer(data
                                  .output('pipe:', format='rawvideo', pix_fmt='rgb24')
                                  .run(quiet=True)[0], np.uint8).reshape([-1, height, width, 3])
+
+    # Initialize output flow
     output = ffmpeg.input('pipe:', format='rawvideo', pix_fmt='rgb24', s='{}x{}'.format(width, height)) \
                    .output(output_path, r=frame_rate).overwrite_output() \
                    .run_async(pipe_stdin=True, quiet=True)
+
+    # Portrait generation process
     model = get_model(model_name)
     for i in range(video_frames.shape[0]):
         img = Image.fromarray(video_frames[i, :, :, :]).convert('RGB')
@@ -254,6 +339,8 @@ def video_portrait(
         else:
             result = (result * 255).astype(np.uint8)
         output.stdin.write(result.tobytes())
+
+    # Write & close flow
     output.stdin.close()
     output.wait()
     return True
